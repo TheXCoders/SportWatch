6916440b68465cc732e7c428a14b4841185a5bb2
Paul Grossiord <th3xcod3rs@protonmail.com>
2020-05-30 12:24:59 +0200

    Change live function for improvments

--- SportWatch/views.py
+++ SportWatch/views.py
@@ -42,9 +42,9 @@ def calendrierMatches(request, comp):
     user=dataPwd[0][0:-1], # [0:-1] for delete the /n char/home/ubuntu/
     passwd=dataPwd[1],
     database="dataAPI",
-    auth_plugin='mysql_native_password'
-)
+    auth_plugin='mysql_native_password')
     cursor = dbAPI.cursor()
+
     if comp == 'cl':
         rqSql = "SELECT * FROM matches_{} WHERE matcheStatus=1 ORDER BY date".format(comp)
     else:
@@ -71,14 +71,50 @@ def calendrierMatches(request, comp):
     else:
         return render(request, "matchs.html", locals())
 
+def getInfosLive(dataDB, comp):
+    #? This function is useful to not duplicate code in live function
+
+    dataArray = list()
+    for m in dataDB:
+        noAffiche = False
+        homeTeam = m[1]
+        awayTeam = m[2]
+
+        if comp == "cl":
+            scoreHome = m[8]
+            awayScore = m[9]
+        else:
+            scoreHome = m[9]
+            awayScore = m[10]
+        idTeam1 = m[4]
+        idTeam2 = m[5]
+        timeMatch = 0
+
+        if m[6] == 3:
+            timeMatch = m[3] + timedelta(hours=2)
+        elif m[6] == 12:
+            timeMatch = "Le match va bientôt débuter !"
+        else:
+            noAffiche = True
+
+        if timeMatch == 'None':
+            if m[6] == 4:
+                timeMatch = "Mi-Temps !"
+            elif m[6] == 10:
+                timeMatch = "Fin du match !"
+
+        if not noAffiche:
+            dataArray.append((homeTeam, awayTeam, idTeam1, idTeam2, scoreHome, awayScore, timeMatch))
+        
+    return dataArray
+
 def live(request, comp):
     dbAPI = mysql.connector.connect(
     host="127.0.0.1",
     user=dataPwd[0][0:-1], # [0:-1] for delete the /n char/home/ubuntu/
     passwd=dataPwd[1],
     database="dataAPI",
-    auth_plugin='mysql_native_password'
-)
+    auth_plugin='mysql_native_password')
     cursor = dbAPI.cursor()
 
     if comp == 'all':
@@ -88,69 +124,14 @@ def live(request, comp):
             cursor.execute(rqSQL)
             data = cursor.fetchall()
 
-            for m in data:
-                noAffiche = False
-                rq = requests.get("https://api.fifa.com/api/v1/live/football/2000000000/0/0/{}".format(m[7]))
-                homeTeam = m[1]
-                awayTeam = m[2]
-                if cp == "cl":
-                    scoreHome = m[8]
-                    awayScore = m[9]
-                else:
-                    scoreHome = m[9]
-                    awayScore = m[10]
-                idTeam1 = m[4]
-                idTeam2 = m[5]
-                timeMatch = 0
-                if m[6] == 3:
-                    timeMatch = str(rq.json()['MatchTime'])
-                elif m[6] == 12:
-                    timeMatch = "Le match va bientôt débuter !"
-                else:
-                    noAffiche = True
-
-                if timeMatch == 'None':
-                    if rq.json()['Period'] == 4:
-                        timeMatch = "Mi-Temps !"
-                    elif rq.json()['Period'] == 10:
-                        timeMatch = "Fin du match !"
-
-                if not noAffiche:
-                    dataMatchs.append((homeTeam, awayTeam, idTeam1, idTeam2, scoreHome, awayScore, timeMatch))
+            dataMatchs += getInfosLive(data, cp)
     else:        
         rqSQL = "SELECT * FROM matches_{} WHERE NOT matcheStatus=0 AND NOT matcheStatus=1 AND NOT matcheStatus=7 ORDER BY date".format(comp)
         cursor.execute(rqSQL)
         data = cursor.fetchall()
         dataMatchs = list()
-        for m in data:
-            rq = requests.get("https://api.fifa.com/api/v1/live/football/2000000000/0/0/{}".format(m[7]))
-            noAffiche = False
-            homeTeam = m[1]
-            awayTeam = m[2]
-            if comp == "cl":
-                scoreHome = m[8]
-                awayScore = m[9]
-            else:
-                scoreHome = m[9]
-                awayScore = m[10]
-            idTeam1 = m[4]
-            idTeam2 = m[5]
-            timeMatch = 0
-            if m[6] == 3:
-                timeMatch = str(rq.json()['MatchTime'])
-            elif m[6] == 12:
-                timeMatch = "Le match va bientôt débuter !"
-            else:
-                noAffiche = True
-
-            if timeMatch == 'None':
-                if rq.json()['Period'] == 4:
-                    timeMatch = "Mi-Temps !"
-                elif rq.json()['Period'] == 10:
-                    timeMatch = "Fin du matche !"
-
-            if not noAffiche:
-                dataMatchs.append((homeTeam, awayTeam, idTeam1, idTeam2, scoreHome, awayScore, timeMatch))
+        
+        dataMatchs += getInfosLive(data, comp)
 
     dbAPI.close()
     
@@ -165,8 +146,7 @@ def resultsMatches(request, comp):
     user=dataPwd[0][0:-1], # [0:-1] for delete the /n char/home/ubuntu/
     passwd=dataPwd[1],
     database="dataAPI",
-    auth_plugin='mysql_native_password'
-)
+    auth_plugin='mysql_native_password')
     cursor = dbAPI.cursor()
 
     if comp == 'cl':
@@ -180,7 +160,7 @@ def resultsMatches(request, comp):
     for m in data:
         homeTeam = m[1]
         awayTeam = m[2]
-        matchTime = m[3] + timedelta(hours=1)
+        matchTime = m[3] + timedelta(hours=2)
         idTeam1 = m[4]
         idTeam2 = m[5]
         if comp == 'cl':
